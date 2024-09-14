4e2d242db2223fb68e86dfe60da69d22bb0455f3
meowmeowahr <meowmeowahr@gmail.com>
2024-09-13 20:15:32 -0500

    Test cleanup

--- tests/test_main_window.py
+++ tests/test_main_window.py
@@ -2,13 +2,17 @@
 Unit tests for main window
 """
 
-import mock
+import pytest
 import queue
 from PySide6.QtWidgets import QApplication
 
 def test_main_window(qtbot):
     import main
 
-    win = main.MainWindow(QApplication.instance(), queue.Queue())
-    assert win.isVisible()
-    win.close()
+    app = QApplication.instance()
+    if app:
+        win = main.MainWindow(app, queue.Queue())
+        assert win.isVisible()
+        win.close()
+    else:
+        pytest.fail("No running QApplication instance found")

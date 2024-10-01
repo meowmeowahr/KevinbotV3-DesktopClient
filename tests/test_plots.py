f54c6369ddce996bc7e95242755161729018bc1b
meowmeowahr <meowmeowahr@gmail.com>
2024-10-01 17:53:35 -0500

    Add test for BatteryGraph

---
+++ tests/test_plots.py
@@ -0,0 +1,17 @@
+"""
+Unit tests for plotting widgets
+"""
+
+
+from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QResizeEvent
+
+from ui.plots import BatteryGraph
+
+
+def test_battery_graph(qtbot):
+    widget = BatteryGraph()
+    widget.set_voltage_range(0, 100)
+    widget.add(50.2)
+    assert widget.values[9] == 50.2
